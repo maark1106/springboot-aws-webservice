8ae25d9e94e909e98ac65ef4acd278bbf42e4a48
maark1106 <mark1106@naver.com>
2024-02-05 10:40:27 +0900

    git init

---
+++ src/main/java/com/jojoldu/book/myspringbootwebservice/MySpringbootWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.myspringbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/myspringbootwebservice/MySpringbootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.myspringbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
