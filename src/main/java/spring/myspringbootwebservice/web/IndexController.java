7c9d4797743d326f34a6112f113afcefb0483cb5
maark1106 <mark1106@naver.com>
2024-02-15 11:40:43 +0900

    index 페이지 생성

---
+++ src/main/java/spring/myspringbootwebservice/web/IndexController.java
@@ -0,0 +1,13 @@
+package spring.myspringbootwebservice.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
