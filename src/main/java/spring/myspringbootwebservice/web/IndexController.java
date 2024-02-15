d08e52f7c6719b5986095c48078f31ccb068ab8d
maark1106 <mark1106@naver.com>
2024-02-15 14:22:30 +0900

    게시글 수정 기능 추가

--- src/main/java/spring/myspringbootwebservice/web/IndexController.java
+++ src/main/java/spring/myspringbootwebservice/web/IndexController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import spring.myspringbootwebservice.service.posts.PostsService;
+import spring.myspringbootwebservice.web.dto.PostsResponseDto;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,5 +18,12 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable("id") Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
